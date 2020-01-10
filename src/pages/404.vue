6e69e4ee9bb7f528f70699a14cee540ef6b8d04d
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-01-11 00:32:25 +0200

    0.0.1

---
+++ src/pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <layout>
+    <section class="flex flex-col items-center justify-center mt-20">
+      <p class="px-12 pb-8 sm:px-0">Ooops - The page you are looking for cannot be found!</p>
+    </section>
+  </layout>
+</template>
