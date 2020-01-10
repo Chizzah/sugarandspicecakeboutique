5e3f2c23b4f40ba9f39f311fbe9d8816e5e7cf77
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-01-11 00:28:02 +0200

    0.0.1

---
+++ src/pages/Success.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <section class="relative h-110 flex flex-col justify-center items-center sm:h-142 sm:text-lg">
+      <div class="w-92 py-8 px-6 bg-green-400 text-gray-200 rounded sm:w-128">
+        <h1 class="pb-2 text-xl font-semibold text-center text-gray-900 sm:text-xl">Your message has been sent succesfully!</h1>
+        <p class="pb-2 text-center">Thank you for contacting me. I will get back to you as soon as possible.</p>
+        <p class="text-gray-900 text-center text-xxs font-semibold"><g-link to="/">Click here to go back to home</g-link></p>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Success'
+  }
+}
+</script>
